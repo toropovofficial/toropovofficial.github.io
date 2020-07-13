e4f25092772a40aa14096d8a27485ee5a2a2de7f
alex <toropovofficial@yandex.ru>
2020-07-13 16:42:57 +0300

    SECONDARY

--- src/js/components/calc.js
+++ src/js/components/calc.js
@@ -1,28 +1,34 @@
 
- //перевод из арабских в римские
-function arabicToRoman(a) {
-  let r = [["I","V"],["X","L"],["C","D"],["M",""]];
-  let f = [[],[[0,1,0]],[[0,2,0]],[[0,3,0]],[[0,1,1],[0,1,0]],[[0,1,1]],
-          [[0,1,0],[0,1,1]],[[0,2,0],[0,1,1]],[[0,3,0],[0,1,1]],[[1,1,0],[0,1,0]]];
-  let num = a; let rim = "";
 
-  String(num).split("").reverse().forEach((element, i) =>  
-    	f[element].forEach((d) => 
-  	     	rim = rim.concat (r[i+d[0]][d[2]].repeat(d[1]))));
-      
-  console.log(rim.split("").reverse().join(""));
-}
-        // перевод из римских в арабские      
-function romanToArabic() {
-    let i, alph = { I:1, V:5, X:10, L:50, C:100, D:500, M:1000 };
-    for(var h = 0, j = 0, n = 0; j < arguments[0].length; j++) {
-        i = alph[arguments[0].charAt(j).toUpperCase()];
-        if(i) n += h = (h < i ? i - (h << 1) : i);
-    }
-    return n;
-}
 
 function calculate(mass) {
+
+     //перевод из арабских в римские
+function arabicToRoman(a) {
+    let r = [["I","V"],["X","L"],["C","D"],["M",""]];
+    let f = [[],[[0,1,0]],[[0,2,0]],[[0,3,0]],[[0,1,1],[0,1,0]],[[0,1,1]],
+            [[0,1,0],[0,1,1]],[[0,2,0],[0,1,1]],[[0,3,0],[0,1,1]],[[1,1,0],[0,1,0]]];
+    let num = a; let rim = "";
+  
+    String(num).split("").reverse().forEach((element, i) =>  
+          f[element].forEach((d) => 
+                 rim = rim.concat (r[i+d[0]][d[2]].repeat(d[1]))));
+        
+    console.log(rim.split("").reverse().join(""));
+  }
+          // перевод из римских в арабские      
+  function romanToArabic() {
+      let i, alph = { I:1, V:5, X:10, L:50, C:100, D:500, M:1000 };
+      for(var h = 0, j = 0, n = 0; j < arguments[0].length; j++) {
+          i = alph[arguments[0].charAt(j).toUpperCase()];
+          if(i) n += h = (h < i ? i - (h << 1) : i);
+      }
+      return n;
+  }
+
+
+
+
     // деление строки на элементы 
     let arr = mass.split(' ');
     if(arr.length < 3) {
@@ -61,6 +67,9 @@ function calculate(mass) {
         // перевод в арабскую систему счисления
         a = romanToArabic(d);
         c = romanToArabic(f);
+        if(a > 10 || c > 10) {
+            return console.log('throws Error');
+        }
     }
     // математические операции и возвращение результата в виде римского числа
     if(b === '+') {
@@ -81,5 +90,5 @@ function calculate(mass) {
         arabicToRoman(str.toFixed());
     }
 }
-calculate('V + V');
+calculate("6 / 2");
 
